7c340da368c01ebdbccffb40f7b4677fd749e292
Rocks <394967886@qq.com>
2017-11-21 19:13:06 +0800

    Model completed 2.0

--- csdn/models.py
+++ csdn/models.py
@@ -1,95 +1,147 @@
 from django.db import models
 
-
 # Create your models here.
-class Artical(models.Model):
-    '''
-    文章
-    '''
-    title = models.CharField(max_length=64, verbose_name='标题')
-    creat_time = models.DateTimeField(auto_now=True, verbose_name='创建日期')
-    summary = models.CharField(max_length=128, verbose_name='摘要')
+from django.contrib.auth.models import AbstractUser
 
 
+class UserInfo(AbstractUser):  # settings:   AUTH_USER_MODEL = "blog.UserInfo"
+    """
+    用户信息
+    """
+    nid = models.BigAutoField(primary_key=True)
+    nickname = models.CharField(verbose_name='昵称', max_length=32)
+    telephone = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号码')
+    avatar = models.FileField(verbose_name='头像', upload_to='avatar', default="/avatar/default.png")
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
 
-
-class Poll(models.Model):
-    '''
-    点赞
-    '''
-    is_positive = models.BooleanField(default=True, verbose_name='点赞或踩')
-    poll_number = models.IntegerField(verbose_name='点赞次数')
-
-
-class Content(models.Model):
-    '''
-    文章内容
-    '''
-    content = models.TextField(verbose_name='文章内容')
-
-
-class Comment(models.Model):
-    '''
-    评论
-    '''
-    comment = models.CharField(max_length=128, verbose_name='评论')
-    comment_time = models.TimeField(auto_now_add=True, verbose_name='评论时间')
-
-
-class CommentPoll(models.Model):
-    '''
-    评论点赞
-    '''
-    is_positive = models.BooleanField(default=True, verbose_name='评论点赞')
-    number = models.IntegerField(verbose_name='次数')
-
-
-class Classify(models.Model):
-    '''
-    文章分类
-    '''
-    Classify_name = models.CharField(verbose_name='分类')
-
-
-class Tag(models.Model):
-    '''
-    标签
-    '''
-    tag_name = models.CharField(verbose_name='标签')
-
-
-class User(models.Model):
-    '''
-    用户表
-    '''
-    username = models.CharField(verbose_name='用户名')
-    password = models.CharField(max_length=64, verbose_name='密码')
-
-
-class UserInfo(models.Model):
-    '''
-    用户详情
-    '''
-    nickname = models.CharField(verbose_name='昵称')
-    age = models.CharField(verbose_name='年龄')
-    sex = models.CharField(verbose_name='性别')
-    email = models.CharField(max_length=64, verbose_name='邮箱地址')
-    job = models.CharField(verbose_name='工作')
-    education = models.CharField(verbose_name='教育')
-    time = models.DateTimeField(auto_now_add=True, verbose_name='创建日期')
-    photo = models.ImageField(verbose_name='头像')
-
-
-class Mail(models.Model):
-    '''
-    站内信
-    '''
-    mail = models.CharField(max_length=128, verbose_name='站内信')
+    def __str__(self):
+        return self.username
 
 
 class Blog(models.Model):
-    '''
-    站点
-    '''
-    blogname = models.CharField(verbose_name='站点名')
-    url = models.CharField(verbose_name='站点路径')
+    """
+    站点信息
+    """
+    nid = models.BigAutoField(primary_key=True)
+    title = models.CharField(verbose_name='个人博客标题', max_length=64)
+    site = models.CharField(verbose_name='个人博客后缀', max_length=32, unique=True)
+    theme = models.CharField(verbose_name='博客主题', max_length=32)
+
+    user = models.OneToOneField(to='UserInfo', to_field='nid')
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """
+    博主个人文章分类表
+    """
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(verbose_name='分类标题', max_length=32)
+    blog = models.ForeignKey(verbose_name='所属博客', to='Blog', to_field='nid')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'category'
+        ordering = ['title']
+
+
+class Article(models.Model):
+    nid = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50, verbose_name='文章标题')
+    desc = models.CharField(max_length=255, verbose_name='文章描述')
+    read_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    up_count = models.IntegerField(default=0)
+    down_count = models.IntegerField(default=0)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+
+    category = models.ForeignKey(verbose_name='文章类型', to='Category', to_field='nid', null=True)
+    user = models.ForeignKey(verbose_name='所属用户', to='UserInfo', to_field='nid')
+    tags = models.ManyToManyField(
+        to="Tag",
+        through='Article2Tag',
+        through_fields=('article', 'tag'),
+    )
+
+    type_choices = [
+        (1, "编程语言"),
+        (2, "软件设计"),
+        (3, "前端"),
+        (4, "操作系统"),
+        (5, "数据库"),
+    ]
+
+    article_type_id = models.IntegerField(choices=type_choices, default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class ArticleDetail(models.Model):
+    """
+    文章详细表
+    """
+    nid = models.AutoField(primary_key=True)
+    content = models.TextField(verbose_name='文章内容', )
+
+    article = models.OneToOneField(verbose_name='所属文章', to='Article', to_field='nid')
+
+
+class Comment(models.Model):
+    """
+    评论表
+    """
+    nid = models.BigAutoField(primary_key=True)
+
+    content = models.CharField(verbose_name='评论内容', max_length=255)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    up_count = models.IntegerField(default=0)
+
+    user = models.ForeignKey(verbose_name='评论者', to='UserInfo', to_field='nid')
+    article = models.ForeignKey(verbose_name='评论文章', to='Article', to_field='nid')
+
+    parent_comment = models.ForeignKey('self', blank=True, null=True, verbose_name='父级评论')
+
+    def __str__(self):
+        return self.content
+
+
+class CommentUp(models.Model):
+    """
+    点赞表
+    """
+
+    nid = models.AutoField(primary_key=True)
+    user = models.ForeignKey('UserInfo', null=True)
+    comment = models.ForeignKey("Comment", null=True)
+
+
+class ArticleUp(models.Model):
+    """
+    点赞表
+    """
+    nid = models.AutoField(primary_key=True)
+    user = models.ForeignKey('UserInfo', null=True)
+    article = models.ForeignKey("Article", null=True)
+
+
+class Tag(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(verbose_name='标签名称', max_length=32)
+    blog = models.ForeignKey(verbose_name='所属博客', to='Blog', to_field='nid')
+
+
+class Article2Tag(models.Model):
+    nid = models.AutoField(primary_key=True)
+    article = models.ForeignKey(verbose_name='文章', to="Article", to_field='nid')
+    tag = models.ForeignKey(verbose_name='标签', to="Tag", to_field='nid')
+
+    class Meta:
+        unique_together = [
+            ('article', 'tag'),
+        ]
--- csdn/views.py
+++ csdn/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 
 # Create your views here.
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    print(username)
+    print(password)
+    flag = False
+    if username == 'rocks' and password == '123':
+        flag = True
+        # return redirect('/index/')
+        import json
+        return HttpResponse(json.dumps(flag))
+    else:
+        import json
+        return HttpResponse(json.dumps(flag))

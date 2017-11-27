53e5bccf1203732b6ef8faa1629bf601984f0028
Rocks <394967886@qq.com>
2017-11-27 19:44:06 +0800

    文章详情页

--- csdn/urls.py
+++ csdn/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings  # 引入settings
 urlpatterns = [
 
     url(r'^(?P<username>.*)/(?P<condition>tag|date|category)/(?P<para>.*)', views.homeSite),
+    url(r'^(?P<username>.*)/articles/(?P<article_id>\d+)', views.articleDetail),
     url(r'^(?P<username>.*)', views.homeSite, name="home"),
 
 ]
